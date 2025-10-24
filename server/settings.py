3f95120ea55372ce6be0a0f9b2d8a5cf1f1185e8
Kapil Arunessh <kapilarunessh@Kapils-MacBook-Air.local>
2025-10-24 13:07:30 +0530

    changed setting file

--- server/settings.py
+++ server/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 # Application definition
 
