2f40d6190a7241454947068e2d4848945194841f
Kapil Arunessh <kapilarunessh@Kapils-MacBook-Air.local>
2025-10-30 21:26:55 +0530

    created a changes in the setting file and add extra lib in the requirements.txt

--- server/settings.py
+++ server/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -151,9 +152,13 @@ USE_TZ = True
 # Static and Media File Handling
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,   'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # URL to access uploaded files (like resumes) in the browser
 MEDIA_URL = '/media/'
