4258c8420f669223f84201a9bc3c662e05d90d28
Kapil Arunessh <kapilarunessh@Kapils-MacBook-Air.local>
2025-12-15 18:16:23 +0530

    chnaged the url in server file for media document

--- server/urls.py
+++ server/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('',include('main.urls')),
 ]
 
-if settings.DEBUG == False:
+if settings.DEBUG :
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
