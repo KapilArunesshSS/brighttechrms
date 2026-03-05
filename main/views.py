e57c3b2a8ac81ba57b834ede4e8e4c7b138ad682
Kapil Arunessh <kapilarunessh@Kapils-MacBook-Air.local>
2026-03-05 22:08:48 +0530

    changed the ffr view and html code

--- main/views.py
+++ main/views.py
@@ -34,7 +34,7 @@ EMAIL_SITE_MAP = {
     "admin.jr@brighttech.net.in": "JAIRAJ",
     "admin.arj@brighttech.net.in": "Arjas",
     "pm.ms@brighttech.net.in": "MSSSL",
-    "admin.agni@brighttech.net.in": "AGNI",
+    "admin.agni@brighttech.net.in": ["AGNI","AGNI-IF"],
 }
 @login_required(login_url='login')
 def FFR(request):
